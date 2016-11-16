fff72623ec71bf85574cc73732dfc89379c334d2
Mario Cecchi <macecchi@gmail.com>
2016-11-16 21:50:51 -0200

    Remove timthumb e adiciona miniaturas estáticas

--- site/inc.instalacoes.php
+++ site/inc.instalacoes.php
@@ -1,32 +1,3 @@
-<?php
-/*
-	# SETTINGS
-	$max_width = 150;
-	$max_height = 150;
-	$dir = "./images/fotos/";
-	
-	function getPictures() {
-		$force = true;
-		global $max_width, $max_height, $dir;
-		if ( $handle = opendir($dir) ) {
-			$lightbox = rand();
-			echo '<ul id="pictures">';
-			while ( ($file = readdir($handle)) !== false ) {
-				if ( !is_dir($file) ) {
-					$split = explode('.', $file); 
-					
-					
-					echo '<li><a href="http://residencialbellavita.com.br/site/images/fotos/'.$file.'" rel="lightbox['.$lightbox.']">';
-					echo '<img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/'.$file.'&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" />';
-					echo '</a></li>';
-				}
-			}
-			echo '</ul>';
-		}
-	}
-*/
-?>
-
 <div class="left">
 <?php include("sidebar.servicos.php"); ?>
 <br />
@@ -36,34 +7,110 @@
 <div class="right">
 <h2>Instalações</h2>
 
-<?php /* getPictures(); */ ?>
-
 <ul id="pictures">
-
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0169.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0169.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0145.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0145.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0147.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0147.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0149.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0149.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0150.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0150.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0151.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0151.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0152.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0152.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0153.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0153.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0155.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0155.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0156.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0156.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0157.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0157.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0158.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0158.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0159.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0159.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0160.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0160.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0161.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0161.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0162.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0162.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0163.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0163.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0164.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0164.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0167.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0167.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-<li><a href="http://residencialbellavita.com.br/site/images/fotos/Foto0168.jpg" rel="lightbox[90376188]"><img src="/timthumb.php?src=http://residencialbellavita.com.br/site/images/fotos/Foto0168.jpg&amp;h=150&amp;w=150&amp;zc=1&amp;q=95" alt="" /></a></li>
-
+	<li>
+		<a href="/site/images/fotos/Foto0169.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0169.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0145.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0145.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0147.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0147.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0149.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0149.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0150.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0150.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0151.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0151.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0152.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0152.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0153.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0153.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0155.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0155.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0156.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0156.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0157.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0157.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0158.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0158.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0159.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0159.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0160.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0160.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0161.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0161.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0162.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0162.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0163.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0163.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0164.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0164.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0167.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0167.jpg">
+		</a>
+	</li>
+	<li>
+		<a href="/site/images/fotos/Foto0168.jpg" rel="lightbox[90376188]">
+			<img src="/site/images/fotos/thumbs/Foto0168.jpg">
+		</a>
+	</li>
 </ul>
 
-<script type="text/javascript" src="http://residencialbellavita.com.br/site/js/prototype.js"></script>
-<script type="text/javascript" src="http://residencialbellavita.com.br/site/js/scriptaculous.js?load=effects,builder"></script>
-<script type="text/javascript" src="http://residencialbellavita.com.br/site/js/lightbox.js"></script>
+<script type="text/javascript" src="/site/js/prototype.js"></script>
+<script type="text/javascript" src="/site/js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="/site/js/lightbox.js"></script>
 </div>
